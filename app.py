be42a4ecd1e3a35dcf9672b54a50e9bb3ec5a705
francooliveraestacion <francooliveraestacion@gmail.com>
2026-09-03 22:16:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ st.sidebar.title("Parametros")
 st.write("Elaborado por :Franco Olivera")
 
 modulos = st.sidebar.selectbox("Selecione el módulo",["Listas", "Arreglos", "Funciones", "POO"])
+
+if modulos == "Listas":
+  st.write("Te encuentras en el módulo de listas")
+
+elif modulos == "Arreglos":
+  st.write("Te encuentras en el módulo de arreglos")
+
+elif modulos == "Funciones":
+  st.write("Te encuentras en el módulo de Funciones")
+  
+else:
+  st.write("Te encuentras en el módulo de POO")
